eee651330337f0dfb9a2243b541076d25b44db97
ivanmilicevic <93290646+ivanmeda993@users.noreply.github.com>
2026-01-01 22:46:20 +0100

    feat(auth): enhance login page with loading state
    - Wrapped LoginForm component in Suspense to display a loading placeholder while the form is being prepared.
    - Updated validation script in package.json to include build step for improved workflow.

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,17 @@
+import { Suspense } from 'react';
+
 import { LoginForm } from '@/module/auth/components/login-form';
 
 const Page = async () => {
-  return <LoginForm />;
+  return (
+    <Suspense
+      fallback={
+        <div className="w-full sm:max-w-md h-[500px] animate-pulse bg-muted rounded-lg" />
+      }
+    >
+      <LoginForm />
+    </Suspense>
+  );
 };
 
 export default Page;
