896fb8beccbac03a628cbfb97fb91506783b87b8
strahinja <strahinjaa20@gmail.com>
2026-01-11 18:02:01 +0100

    fix(about): correct import paths for About components
    - Updated import paths for AboutCta, AboutHero, AboutTeam, AboutTimeline, and AboutValues components.
    - Ensured consistency in module structure for better maintainability.

--- app/(public)/o-nama/page.tsx
+++ app/(public)/o-nama/page.tsx
@@ -1,8 +1,8 @@
-import AboutCta from '@/module/public/services/views/about-view/cta';
-import AboutHero from '@/module/public/services/views/about-view/hero';
-import AboutTeam from '@/module/public/services/views/about-view/team';
-import AboutTimeline from '@/module/public/services/views/about-view/timeline';
-import AboutValues from '@/module/public/services/views/about-view/values';
+import AboutCta from '@/module/public/about/views/about-view/cta';
+import AboutHero from '@/module/public/about/views/about-view/hero';
+import AboutTeam from '@/module/public/about/views/about-view/team';
+import AboutTimeline from '@/module/public/about/views/about-view/timeline';
+import AboutValues from '@/module/public/about/views/about-view/values';
 
 export default function AboutPage() {
   return (
